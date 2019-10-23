83a37b36f092ff1203f1ad4230a3e06ca976727f
binieth <abigiyabiniyam@gmail.com>
2019-10-23 13:23:15 -0400

    cleaner javascript and css format than the previous.

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-var randomNumber1 = Math.floor(Math.random()*6)+1;
-var randomDiceImage = "dice"+randomNumber1+".png";
-var randomImageSource = "images/"+randomDiceImage;
-var image1 = document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
 
-var randomNumber2 = Math.floor(Math.random()*6)+1;
-var randomDiceImage2 = "dice"+randomNumber2+".png";
-var randomImageSource2 = "images/"+randomDiceImage2;
-var image2 = document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+var playerOne = document.getElementsByTagName('img')[0].setAttribute('src', 'images/dice'+randomNumber1+'.png');
+var playerTwo = document.getElementsByTagName('img')[1].setAttribute('src', 'images/dice'+randomNumber2+'.png');
 
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🎉 Player 1 Win!";
-} else if (randomNumber2 > randomNumber1){
-    document.querySelector("h1").innerHTML = "🎉 Player 2 Win!";
-} else {
-    document.querySelector("h1").innerHTML = " Draw!";
+function diceGame() {
+    if( randomNumber1 > randomNumber2){
+        document.querySelector('h1').innerHTML = "Player 1 Wins!"
+    } else if( randomNumber1 < randomNumber2){
+        document.querySelector('h1').innerHTML = "Player 2 Wins!"
+    } else {
+        document.querySelector('h1').innerHTML = "Draw!"
+    }
 }
 
+diceGame();
